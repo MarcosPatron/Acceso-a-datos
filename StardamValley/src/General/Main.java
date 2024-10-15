411548490dfea1dfde86a011031aab131296949e
MarcosPatron <152577391+MarcosPatron@users.noreply.github.com>
2024-10-15 20:22:12 +0200

    Stardew Vallye

---
+++ StardamValley/src/General/Main.java
@@ -0,0 +1,26 @@
+package General;
+
+public class Main {
+
+    public static void menuInicio(){
+        System.out.println("BINEVENIDO A STARDAM VALLEY" +
+                "\n--------------------------------" +
+                "\n1. NUEVA PARTIDA" +
+                "\n2. CARGAR PARTIDA");
+    }
+
+    public static void menuFuncionalidades(){
+        System.out.println("STARDAM VALLEY" +
+                "\n1. INICIAR NUEVO DIA" +
+                "\n2. ATENDER CULTIVOS" +
+                "\n3. PLANTAR CULTIVOS EN COLUMNA" +
+                "\n4. VENDER COSECHA" +
+                "\n5. MOSTRAR INFORMACION DE LA GRANJA" +
+                "\n6. SALIR");
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ StardamValley/src/Utils/PropertiesF.java
@@ -0,0 +1,17 @@
+package Utils;
+
+public class PropertiesF {
+
+    public static void inicializar(){
+
+    }
+
+    public static void leer(){
+
+    }
+
+    public static void escribir(){
+
+    }
+
+}
