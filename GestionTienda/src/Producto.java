3c9baec01af9be9a82940e10342f14627775c7e5
Marcos Patrón Barriga <152577391+MarcosPatron@users.noreply.github.com>
2024-10-28 20:10:00 +0100

    Ejercicio Tienda

---
+++ GestionTienda/src/Producto.java
@@ -0,0 +1,64 @@
+public class Producto {
+
+    private int id;
+    private String nombre;
+    private double precio;
+    private int idFabricante;
+    private String nombreFabicante;
+
+    public Producto(int id, String nombre, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.idFabricante = -1;
+        this.nombreFabicante = "";
+    }
+
+    public Producto(int id, String nombre, double precio, int idFabricante, java.lang.String nombreFabicante) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.idFabricante = idFabricante;
+        this.nombreFabicante = nombreFabicante;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getIdFabricante() {
+        return idFabricante;
+    }
+
+    public void setIdFabricante(int idFabricante) {
+        this.idFabricante = idFabricante;
+    }
+
+    public String getNombreFabicante() {
+        return nombreFabicante;
+    }
+
+    public void setNombreFabicante(String nombreFabicante) {
+        this.nombreFabicante = nombreFabicante;
+    }
+}
